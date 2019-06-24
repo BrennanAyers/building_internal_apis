537876b56e8de45f796e352b7781f67704904bbb
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-24 15:25:24 -0600

    Add alternative syntax for Destroy spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -57,5 +57,12 @@ describe "Items API" do
     expect(response).to be_successful
     expect(Item.count).to eq(0)
     expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+
+    item = create(:item)
+
+   expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+
+   expect(response).to be_success
+   expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end

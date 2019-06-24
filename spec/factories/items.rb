a831667652df9a5aba348246a02016d5bd0fa454
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-24 14:21:46 -0600

    Update Item factory to be interesting

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
+    name { "Banana Stand" }
+    description { "Always money in the business, that Banana Stand budget" }
   end
 end

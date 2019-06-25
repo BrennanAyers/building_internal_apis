47bd21b55edc7173dd434eedfcf1573a1d70ed4d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-25 11:36:16 -0600

    Fix tab indentation on Item Request #destroy spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -60,9 +60,9 @@ describe "Items API" do
 
     item = create(:item)
 
-   expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+    expect{ delete "/api/v1/items/#{item.id}" }.to  change(Item, :count).by(-1)
 
-   expect(response).to be_success
-   expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    expect(response).to be_success
+    expect{Item.find(item.id)}.to  raise_error(ActiveRecord::RecordNotFound)
   end
 end
